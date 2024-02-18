f6ec01b9993cbeb095e83f9a64c618f73238bfa3
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-18 18:34:08 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Maya Ghamloush on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
